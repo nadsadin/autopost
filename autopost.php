a4e5abbf09f5d1382704e4b6b23b347728d21278
Actual Task <actualtask@gmail.com>
2017-08-29 11:50:03 +0300

    add functions from WP

---
+++ autopost.php
@@ -0,0 +1,500 @@
+<?php
+function autop( $pee, $br = true ) {
+	$pre_tags = array();
+
+	if ( trim($pee) === '' )
+		return '';
+
+	// Just to make things a little easier, pad the end.
+	$pee = $pee . "\n";
+
+	/*
+	 * Pre tags shouldn't be touched by autop.
+	 * Replace pre tags with placeholders and bring them back after autop.
+	 */
+	if ( strpos($pee, '<pre') !== false ) {
+		$pee_parts = explode( '</pre>', $pee );
+		$last_pee = array_pop($pee_parts);
+		$pee = '';
+		$i = 0;
+
+		foreach ( $pee_parts as $pee_part ) {
+			$start = strpos($pee_part, '<pre');
+
+			// Malformed html?
+			if ( $start === false ) {
+				$pee .= $pee_part;
+				continue;
+			}
+
+			$name = "<pre wp-pre-tag-$i></pre>";
+			$pre_tags[$name] = substr( $pee_part, $start ) . '</pre>';
+
+			$pee .= substr( $pee_part, 0, $start ) . $name;
+			$i++;
+		}
+
+		$pee .= $last_pee;
+	}
+	// Change multiple <br>s into two line breaks, which will turn into paragraphs.
+	$pee = preg_replace('|<br\s*/?>\s*<br\s*/?>|', "\n\n", $pee);
+
+	$allblocks = '(?:table|thead|tfoot|caption|col|colgroup|tbody|tr|td|th|div|dl|dd|dt|ul|ol|li|pre|form|map|area|blockquote|address|math|style|p|h[1-6]|hr|fieldset|legend|section|article|aside|hgroup|header|footer|nav|figure|figcaption|details|menu|summary)';
+
+	// Add a double line break above block-level opening tags.
+	$pee = preg_replace('!(<' . $allblocks . '[\s/>])!', "\n\n$1", $pee);
+
+	// Add a double line break below block-level closing tags.
+	$pee = preg_replace('!(</' . $allblocks . '>)!', "$1\n\n", $pee);
+
+	// Standardize newline characters to "\n".
+	$pee = str_replace(array("\r\n", "\r"), "\n", $pee);
+
+	// Find newlines in all elements and add placeholders.
+	$pee = wp_replace_in_html_tags( $pee, array( "\n" => " <!-- wpnl --> " ) );
+
+	// Collapse line breaks before and after <option> elements so they don't get autop'd.
+	if ( strpos( $pee, '<option' ) !== false ) {
+		$pee = preg_replace( '|\s*<option|', '<option', $pee );
+		$pee = preg_replace( '|</option>\s*|', '</option>', $pee );
+	}
+
+	/*
+	 * Collapse line breaks inside <object> elements, before <param> and <embed> elements
+	 * so they don't get autop'd.
+	 */
+	if ( strpos( $pee, '</object>' ) !== false ) {
+		$pee = preg_replace( '|(<object[^>]*>)\s*|', '$1', $pee );
+		$pee = preg_replace( '|\s*</object>|', '</object>', $pee );
+		$pee = preg_replace( '%\s*(</?(?:param|embed)[^>]*>)\s*%', '$1', $pee );
+	}
+
+	/*
+	 * Collapse line breaks inside <audio> and <video> elements,
+	 * before and after <source> and <track> elements.
+	 */
+	if ( strpos( $pee, '<source' ) !== false || strpos( $pee, '<track' ) !== false ) {
+		$pee = preg_replace( '%([<\[](?:audio|video)[^>\]]*[>\]])\s*%', '$1', $pee );
+		$pee = preg_replace( '%\s*([<\[]/(?:audio|video)[>\]])%', '$1', $pee );
+		$pee = preg_replace( '%\s*(<(?:source|track)[^>]*>)\s*%', '$1', $pee );
+	}
+
+	// Collapse line breaks before and after <figcaption> elements.
+	if ( strpos( $pee, '<figcaption' ) !== false ) {
+		$pee = preg_replace( '|\s*(<figcaption[^>]*>)|', '$1', $pee );
+		$pee = preg_replace( '|</figcaption>\s*|', '</figcaption>', $pee );
+	}
+
+	// Remove more than two contiguous line breaks.
+	$pee = preg_replace("/\n\n+/", "\n\n", $pee);
+
+	// Split up the contents into an array of strings, separated by double line breaks.
+	$pees = preg_split('/\n\s*\n/', $pee, -1, PREG_SPLIT_NO_EMPTY);
+
+	// Reset $pee prior to rebuilding.
+	$pee = '';
+
+	// Rebuild the content as a string, wrapping every bit with a <p>.
+	foreach ( $pees as $tinkle ) {
+		$pee .= '<p>' . trim($tinkle, "\n") . "</p>\n";
+	}
+
+	// Under certain strange conditions it could create a P of entirely whitespace.
+	$pee = preg_replace('|<p>\s*</p>|', '', $pee);
+
+	// Add a closing <p> inside <div>, <address>, or <form> tag if missing.
+	$pee = preg_replace('!<p>([^<]+)</(div|address|form)>!', "<p>$1</p></$2>", $pee);
+
+	// If an opening or closing block element tag is wrapped in a <p>, unwrap it.
+	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+
+	// In some cases <li> may get wrapped in <p>, fix them.
+	$pee = preg_replace("|<p>(<li.+?)</p>|", "$1", $pee);
+
+	// If a <blockquote> is wrapped with a <p>, move it inside the <blockquote>.
+	$pee = preg_replace('|<p><blockquote([^>]*)>|i', "<blockquote$1><p>", $pee);
+	$pee = str_replace('</blockquote></p>', '</p></blockquote>', $pee);
+
+	// If an opening or closing block element tag is preceded by an opening <p> tag, remove it.
+	$pee = preg_replace('!<p>\s*(</?' . $allblocks . '[^>]*>)!', "$1", $pee);
+
+	// If an opening or closing block element tag is followed by a closing <p> tag, remove it.
+	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*</p>!', "$1", $pee);
+
+	// Optionally insert line breaks.
+	if ( $br ) {
+		// Replace newlines that shouldn't be touched with a placeholder.
+		$pee = preg_replace_callback('/<(script|style).*?<\/\\1>/s', '_autop_newline_preservation_helper', $pee);
+
+		// Normalize <br>
+		$pee = str_replace( array( '<br>', '<br/>' ), '<br />', $pee );
+
+		// Replace any new line characters that aren't preceded by a <br /> with a <br />.
+		$pee = preg_replace('|(?<!<br />)\s*\n|', "<br />\n", $pee);
+
+		// Replace newline placeholders with newlines.
+		$pee = str_replace('<WPPreserveNewline />', "\n", $pee);
+	}
+
+	// If a <br /> tag is after an opening or closing block tag, remove it.
+	$pee = preg_replace('!(</?' . $allblocks . '[^>]*>)\s*<br />!', "$1", $pee);
+
+	// If a <br /> tag is before a subset of opening or closing block tags, remove it.
+	$pee = preg_replace('!<br />(\s*</?(?:p|li|div|dl|dd|dt|th|pre|td|ul|ol)[^>]*>)!', '$1', $pee);
+	$pee = preg_replace( "|\n</p>$|", '</p>', $pee );
+
+	// Replace placeholder <pre> tags with their original content.
+	if ( !empty($pre_tags) )
+		$pee = str_replace(array_keys($pre_tags), array_values($pre_tags), $pee);
+
+	// Restore newlines in all elements.
+	if ( false !== strpos( $pee, '<!-- wpnl -->' ) ) {
+		$pee = str_replace( array( ' <!-- wpnl --> ', '<!-- wpnl -->' ), "\n", $pee );
+	}
+
+	return $pee;
+}
+function remove_accents( $string ) {
+    if ( !preg_match('/[\x80-\xff]/', $string) )
+        return $string;
+
+    if (seems_utf8($string)) {
+        $chars = array(
+            // Decompositions for Latin-1 Supplement
+            'ª' => 'a', 'º' => 'o',
+            'À' => 'A', 'Á' => 'A',
+            'Â' => 'A', 'Ã' => 'A',
+            'Ä' => 'A', 'Å' => 'A',
+            'Æ' => 'AE','Ç' => 'C',
+            'È' => 'E', 'É' => 'E',
+            'Ê' => 'E', 'Ë' => 'E',
+            'Ì' => 'I', 'Í' => 'I',
+            'Î' => 'I', 'Ï' => 'I',
+            'Ð' => 'D', 'Ñ' => 'N',
+            'Ò' => 'O', 'Ó' => 'O',
+            'Ô' => 'O', 'Õ' => 'O',
+            'Ö' => 'O', 'Ù' => 'U',
+            'Ú' => 'U', 'Û' => 'U',
+            'Ü' => 'U', 'Ý' => 'Y',
+            'Þ' => 'TH','ß' => 's',
+            'à' => 'a', 'á' => 'a',
+            'â' => 'a', 'ã' => 'a',
+            'ä' => 'a', 'å' => 'a',
+            'æ' => 'ae','ç' => 'c',
+            'è' => 'e', 'é' => 'e',
+            'ê' => 'e', 'ë' => 'e',
+            'ì' => 'i', 'í' => 'i',
+            'î' => 'i', 'ï' => 'i',
+            'ð' => 'd', 'ñ' => 'n',
+            'ò' => 'o', 'ó' => 'o',
+            'ô' => 'o', 'õ' => 'o',
+            'ö' => 'o', 'ø' => 'o',
+            'ù' => 'u', 'ú' => 'u',
+            'û' => 'u', 'ü' => 'u',
+            'ý' => 'y', 'þ' => 'th',
+            'ÿ' => 'y', 'Ø' => 'O',
+            // Decompositions for Latin Extended-A
+            'Ā' => 'A', 'ā' => 'a',
+            'Ă' => 'A', 'ă' => 'a',
+            'Ą' => 'A', 'ą' => 'a',
+            'Ć' => 'C', 'ć' => 'c',
+            'Ĉ' => 'C', 'ĉ' => 'c',
+            'Ċ' => 'C', 'ċ' => 'c',
+            'Č' => 'C', 'č' => 'c',
+            'Ď' => 'D', 'ď' => 'd',
+            'Đ' => 'D', 'đ' => 'd',
+            'Ē' => 'E', 'ē' => 'e',
+            'Ĕ' => 'E', 'ĕ' => 'e',
+            'Ė' => 'E', 'ė' => 'e',
+            'Ę' => 'E', 'ę' => 'e',
+            'Ě' => 'E', 'ě' => 'e',
+            'Ĝ' => 'G', 'ĝ' => 'g',
+            'Ğ' => 'G', 'ğ' => 'g',
+            'Ġ' => 'G', 'ġ' => 'g',
+            'Ģ' => 'G', 'ģ' => 'g',
+            'Ĥ' => 'H', 'ĥ' => 'h',
+            'Ħ' => 'H', 'ħ' => 'h',
+            'Ĩ' => 'I', 'ĩ' => 'i',
+            'Ī' => 'I', 'ī' => 'i',
+            'Ĭ' => 'I', 'ĭ' => 'i',
+            'Į' => 'I', 'į' => 'i',
+            'İ' => 'I', 'ı' => 'i',
+            'Ĳ' => 'IJ','ĳ' => 'ij',
+            'Ĵ' => 'J', 'ĵ' => 'j',
+            'Ķ' => 'K', 'ķ' => 'k',
+            'ĸ' => 'k', 'Ĺ' => 'L',
+            'ĺ' => 'l', 'Ļ' => 'L',
+            'ļ' => 'l', 'Ľ' => 'L',
+            'ľ' => 'l', 'Ŀ' => 'L',
+            'ŀ' => 'l', 'Ł' => 'L',
+            'ł' => 'l', 'Ń' => 'N',
+            'ń' => 'n', 'Ņ' => 'N',
+            'ņ' => 'n', 'Ň' => 'N',
+            'ň' => 'n', 'ŉ' => 'n',
+            'Ŋ' => 'N', 'ŋ' => 'n',
+            'Ō' => 'O', 'ō' => 'o',
+            'Ŏ' => 'O', 'ŏ' => 'o',
+            'Ő' => 'O', 'ő' => 'o',
+            'Œ' => 'OE','œ' => 'oe',
+            'Ŕ' => 'R','ŕ' => 'r',
+            'Ŗ' => 'R','ŗ' => 'r',
+            'Ř' => 'R','ř' => 'r',
+            'Ś' => 'S','ś' => 's',
+            'Ŝ' => 'S','ŝ' => 's',
+            'Ş' => 'S','ş' => 's',
+            'Š' => 'S', 'š' => 's',
+            'Ţ' => 'T', 'ţ' => 't',
+            'Ť' => 'T', 'ť' => 't',
+            'Ŧ' => 'T', 'ŧ' => 't',
+            'Ũ' => 'U', 'ũ' => 'u',
+            'Ū' => 'U', 'ū' => 'u',
+            'Ŭ' => 'U', 'ŭ' => 'u',
+            'Ů' => 'U', 'ů' => 'u',
+            'Ű' => 'U', 'ű' => 'u',
+            'Ų' => 'U', 'ų' => 'u',
+            'Ŵ' => 'W', 'ŵ' => 'w',
+            'Ŷ' => 'Y', 'ŷ' => 'y',
+            'Ÿ' => 'Y', 'Ź' => 'Z',
+            'ź' => 'z', 'Ż' => 'Z',
+            'ż' => 'z', 'Ž' => 'Z',
+            'ž' => 'z', 'ſ' => 's',
+            // Decompositions for Latin Extended-B
+            'Ș' => 'S', 'ș' => 's',
+            'Ț' => 'T', 'ț' => 't',
+            // Euro Sign
+            '€' => 'E',
+            // GBP (Pound) Sign
+            '£' => '',
+            // Vowels with diacritic (Vietnamese)
+            // unmarked
+            'Ơ' => 'O', 'ơ' => 'o',
+            'Ư' => 'U', 'ư' => 'u',
+            // grave accent
+            'Ầ' => 'A', 'ầ' => 'a',
+            'Ằ' => 'A', 'ằ' => 'a',
+            'Ề' => 'E', 'ề' => 'e',
+            'Ồ' => 'O', 'ồ' => 'o',
+            'Ờ' => 'O', 'ờ' => 'o',
+            'Ừ' => 'U', 'ừ' => 'u',
+            'Ỳ' => 'Y', 'ỳ' => 'y',
+            // hook
+            'Ả' => 'A', 'ả' => 'a',
+            'Ẩ' => 'A', 'ẩ' => 'a',
+            'Ẳ' => 'A', 'ẳ' => 'a',
+            'Ẻ' => 'E', 'ẻ' => 'e',
+            'Ể' => 'E', 'ể' => 'e',
+            'Ỉ' => 'I', 'ỉ' => 'i',
+            'Ỏ' => 'O', 'ỏ' => 'o',
+            'Ổ' => 'O', 'ổ' => 'o',
+            'Ở' => 'O', 'ở' => 'o',
+            'Ủ' => 'U', 'ủ' => 'u',
+            'Ử' => 'U', 'ử' => 'u',
+            'Ỷ' => 'Y', 'ỷ' => 'y',
+            // tilde
+            'Ẫ' => 'A', 'ẫ' => 'a',
+            'Ẵ' => 'A', 'ẵ' => 'a',
+            'Ẽ' => 'E', 'ẽ' => 'e',
+            'Ễ' => 'E', 'ễ' => 'e',
+            'Ỗ' => 'O', 'ỗ' => 'o',
+            'Ỡ' => 'O', 'ỡ' => 'o',
+            'Ữ' => 'U', 'ữ' => 'u',
+            'Ỹ' => 'Y', 'ỹ' => 'y',
+            // acute accent
+            'Ấ' => 'A', 'ấ' => 'a',
+            'Ắ' => 'A', 'ắ' => 'a',
+            'Ế' => 'E', 'ế' => 'e',
+            'Ố' => 'O', 'ố' => 'o',
+            'Ớ' => 'O', 'ớ' => 'o',
+            'Ứ' => 'U', 'ứ' => 'u',
+            // dot below
+            'Ạ' => 'A', 'ạ' => 'a',
+            'Ậ' => 'A', 'ậ' => 'a',
+            'Ặ' => 'A', 'ặ' => 'a',
+            'Ẹ' => 'E', 'ẹ' => 'e',
+            'Ệ' => 'E', 'ệ' => 'e',
+            'Ị' => 'I', 'ị' => 'i',
+            'Ọ' => 'O', 'ọ' => 'o',
+            'Ộ' => 'O', 'ộ' => 'o',
+            'Ợ' => 'O', 'ợ' => 'o',
+            'Ụ' => 'U', 'ụ' => 'u',
+            'Ự' => 'U', 'ự' => 'u',
+            'Ỵ' => 'Y', 'ỵ' => 'y',
+            // Vowels with diacritic (Chinese, Hanyu Pinyin)
+            'ɑ' => 'a',
+            // macron
+            'Ǖ' => 'U', 'ǖ' => 'u',
+            // acute accent
+            'Ǘ' => 'U', 'ǘ' => 'u',
+            // caron
+            'Ǎ' => 'A', 'ǎ' => 'a',
+            'Ǐ' => 'I', 'ǐ' => 'i',
+            'Ǒ' => 'O', 'ǒ' => 'o',
+            'Ǔ' => 'U', 'ǔ' => 'u',
+            'Ǚ' => 'U', 'ǚ' => 'u',
+            // grave accent
+            'Ǜ' => 'U', 'ǜ' => 'u',
+        );
+
+//        // Used for locale-specific rules
+//        $locale = get_locale();
+//
+//        if ( 'de_DE' == $locale || 'de_DE_formal' == $locale || 'de_CH' == $locale || 'de_CH_informal' == $locale ) {
+//            $chars[ 'Ä' ] = 'Ae';
+//            $chars[ 'ä' ] = 'ae';
+//            $chars[ 'Ö' ] = 'Oe';
+//            $chars[ 'ö' ] = 'oe';
+//            $chars[ 'Ü' ] = 'Ue';
+//            $chars[ 'ü' ] = 'ue';
+//            $chars[ 'ß' ] = 'ss';
+//        } elseif ( 'da_DK' === $locale ) {
+//            $chars[ 'Æ' ] = 'Ae';
+//            $chars[ 'æ' ] = 'ae';
+//            $chars[ 'Ø' ] = 'Oe';
+//            $chars[ 'ø' ] = 'oe';
+//            $chars[ 'Å' ] = 'Aa';
+//            $chars[ 'å' ] = 'aa';
+//        } elseif ( 'ca' === $locale ) {
+//            $chars[ 'l·l' ] = 'll';
+//        } elseif ( 'sr_RS' === $locale || 'bs_BA' === $locale ) {
+//            $chars[ 'Đ' ] = 'DJ';
+//            $chars[ 'đ' ] = 'dj';
+//        }
+
+        $string = strtr($string, $chars);
+    } else {
+        $chars = array();
+        // Assume ISO-8859-1 if not UTF-8
+        $chars['in'] = "\x80\x83\x8a\x8e\x9a\x9e"
+            ."\x9f\xa2\xa5\xb5\xc0\xc1\xc2"
+            ."\xc3\xc4\xc5\xc7\xc8\xc9\xca"
+            ."\xcb\xcc\xcd\xce\xcf\xd1\xd2"
+            ."\xd3\xd4\xd5\xd6\xd8\xd9\xda"
+            ."\xdb\xdc\xdd\xe0\xe1\xe2\xe3"
+            ."\xe4\xe5\xe7\xe8\xe9\xea\xeb"
+            ."\xec\xed\xee\xef\xf1\xf2\xf3"
+            ."\xf4\xf5\xf6\xf8\xf9\xfa\xfb"
+            ."\xfc\xfd\xff";
+
+        $chars['out'] = "EfSZszYcYuAAAAAACEEEEIIIINOOOOOOUUUUYaaaaaaceeeeiiiinoooooouuuuyy";
+
+        $string = strtr($string, $chars['in'], $chars['out']);
+        $double_chars = array();
+        $double_chars['in'] = array("\x8c", "\x9c", "\xc6", "\xd0", "\xde", "\xdf", "\xe6", "\xf0", "\xfe");
+        $double_chars['out'] = array('OE', 'oe', 'AE', 'DH', 'TH', 'ss', 'ae', 'dh', 'th');
+        $string = str_replace($double_chars['in'], $double_chars['out'], $string);
+    }
+
+    return $string;
+}
+function seems_utf8( $str ) {
+    mbstring_binary_safe_encoding();
+    $length = strlen($str);
+    reset_mbstring_encoding();
+    for ($i=0; $i < $length; $i++) {
+        $c = ord($str[$i]);
+        if ($c < 0x80) $n = 0; // 0bbbbbbb
+        elseif (($c & 0xE0) == 0xC0) $n=1; // 110bbbbb
+        elseif (($c & 0xF0) == 0xE0) $n=2; // 1110bbbb
+        elseif (($c & 0xF8) == 0xF0) $n=3; // 11110bbb
+        elseif (($c & 0xFC) == 0xF8) $n=4; // 111110bb
+        elseif (($c & 0xFE) == 0xFC) $n=5; // 1111110b
+        else return false; // Does not match any model
+        for ($j=0; $j<$n; $j++) { // n bytes matching 10bbbbbb follow ?
+            if ((++$i == $length) || ((ord($str[$i]) & 0xC0) != 0x80))
+                return false;
+        }
+    }
+    return true;
+}
+function mbstring_binary_safe_encoding( $reset = false ) {
+    static $encodings = array();
+    static $overloaded = null;
+
+    if ( is_null( $overloaded ) )
+        $overloaded = function_exists( 'mb_internal_encoding' ) && ( ini_get( 'mbstring.func_overload' ) & 2 );
+
+    if ( false === $overloaded )
+        return;
+
+    if ( ! $reset ) {
+        $encoding = mb_internal_encoding();
+        array_push( $encodings, $encoding );
+        mb_internal_encoding( 'ISO-8859-1' );
+    }
+
+    if ( $reset && $encodings ) {
+        $encoding = array_pop( $encodings );
+        mb_internal_encoding( $encoding );
+    }
+}
+function reset_mbstring_encoding() {
+    mbstring_binary_safe_encoding( true );
+}
+function sanitize_title_with_dashes( $title, $raw_title = '', $context = 'save' ) {
+	$title = strip_tags($title);
+	// Preserve escaped octets.
+	$title = preg_replace('|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title);
+	// Remove percent signs that are not part of an octet.
+	$title = str_replace('%', '', $title);
+	// Restore octets.
+	$title = preg_replace('|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title);
+
+	if (seems_utf8($title)) {
+		if (function_exists('mb_strtolower')) {
+			$title = mb_strtolower($title, 'UTF-8');
+		}
+		$title = utf8_uri_encode($title, 200);
+	}
+
+	$title = strtolower($title);
+
+	if ( 'save' == $context ) {
+		// Convert nbsp, ndash and mdash to hyphens
+		$title = str_replace( array( '%c2%a0', '%e2%80%93', '%e2%80%94' ), '-', $title );
+		// Convert nbsp, ndash and mdash HTML entities to hyphens
+		$title = str_replace( array( '&nbsp;', '&#160;', '&ndash;', '&#8211;', '&mdash;', '&#8212;' ), '-', $title );
+
+		// Strip these characters entirely
+		$title = str_replace( array(
+			// iexcl and iquest
+			'%c2%a1', '%c2%bf',
+			// angle quotes
+			'%c2%ab', '%c2%bb', '%e2%80%b9', '%e2%80%ba',
+			// curly quotes
+			'%e2%80%98', '%e2%80%99', '%e2%80%9c', '%e2%80%9d',
+			'%e2%80%9a', '%e2%80%9b', '%e2%80%9e', '%e2%80%9f',
+			// copy, reg, deg, hellip and trade
+			'%c2%a9', '%c2%ae', '%c2%b0', '%e2%80%a6', '%e2%84%a2',
+			// acute accents
+			'%c2%b4', '%cb%8a', '%cc%81', '%cd%81',
+			// grave accent, macron, caron
+			'%cc%80', '%cc%84', '%cc%8c',
+		), '', $title );
+
+		// Convert times to x
+		$title = str_replace( '%c3%97', 'x', $title );
+	}
+
+	$title = preg_replace('/&.+?;/', '', $title); // kill entities
+	$title = str_replace('.', '-', $title);
+
+	$title = preg_replace('/[^%a-z0-9 _-]/', '', $title);
+	$title = preg_replace('/\s+/', '-', $title);
+	$title = preg_replace('|-+|', '-', $title);
+	$title = trim($title, '-');
+
+	return $title;
+}
+function transliterate($st) {
+  $st = strtr($st,
+    "абвгдежзийклмнопрстуфыэАБВГДЕЖЗИЙКЛМНОПРСТУФЫЭ",
+    "abvgdegziyklmnoprstufieABVGDEGZIYKLMNOPRSTUFIE"
+  );
+  $st = strtr($st, array(
+    'ё'=>"yo",    'х'=>"h",  'ц'=>"ts",  'ч'=>"ch", 'ш'=>"sh",
+    'щ'=>"shch",  'ъ'=>'',   'ь'=>'',    'ю'=>"yu", 'я'=>"ya",
+    'Ё'=>"Yo",    'Х'=>"H",  'Ц'=>"Ts",  'Ч'=>"Ch", 'Ш'=>"Sh",
+    'Щ'=>"Shch",  'Ъ'=>'',   'Ь'=>'',    'Ю'=>"Yu", 'Я'=>"Ya", ' '=>"_"
+  ));
+  return $st;
+}
---
+++ autopost_admin.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Autopost Admin</h1>
+
+<form method="post">
+
+</form>
+
+</body>
+</html>
